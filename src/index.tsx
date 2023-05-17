cc3e374a5deb54002b9c926c1d7741ccddee307f
Duong Nguyen <dnguyenlearning@gmail.com>
2023-05-17 15:37:48 +0700

    based react-block-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const HelloWorld: React.FC = () => {
-  return (
-    <div>
-      Hello World Duong
-      <Testing />
-    </div>
-  );
+  return <div>Hello World Duong</div>;
 };
 
 export default HelloWorld;
