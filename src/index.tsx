bfe887887cf1bb8b6e4279202ec3a718f467c5ab
Duong Nguyen <dnguyenlearning@gmail.com>
2023-05-17 23:06:36 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const HelloWorld: React.FC = () => {
-  return <div>Hello World Duong</div>;
+const App: React.FC = () => {
+  return <div>Hello World</div>;
 };
 
-export default HelloWorld;
+export default App;
